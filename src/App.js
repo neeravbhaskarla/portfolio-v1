949e649b856e25a6483542619e53d1aaa03622fb
neeravbhaskarla <ananthaneerav@gmail.com>
2022-12-20 21:15:35 +0530

    fixed mobile bugs, which includes layouts, navbar fix and removing cursor in case of mobile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import MainPage from './components/MainPage'
 import './App.scss'
+import { isMobile } from 'react-device-detect'
 
 export default function App() {
   return (
       <div className="main-page">
-        <div class="cursor"></div>
+        {isMobile? null: <div class="cursor"></div>}
         <MainPage/>
       </div>
   )
